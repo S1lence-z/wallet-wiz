e39c7211ea41955ec2f5573179a0e354ee1591fd
S1lence-z <j3.zelenka@gmail.com>
2024-11-18 15:07:52 +0100

    chore: upgrade compile sdk, target sdk and dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.walletwiz"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.walletwiz"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
